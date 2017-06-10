d51238916413cfaff8e0720a9462545f7cea9a5a
Sean <s.frankruyter@gmail.com>
2017-06-10 15:01:14 -0400

    added font-awesome spells can be added / removed from bookmarks ui for expanding / collapsing spell info added

--- src/components/SpellList.vue
+++ src/components/SpellList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-      <spell v-for="(spell, index) in spells" v-bind:spell="spell" v-bind:key="index"></spell>
+      <spell v-for="(spell, index) in spells" v-bind:spell="spell" v-bind:key="index" v-on:add-bookmark="addBookmark" v-on:remove-bookmark="removeBookmark"></spell>
   </div>
 </template>
 
@@ -9,13 +9,29 @@ import Spell from './Spell'
 
 export default {
   props: ['spells'],
+  data () {
+    return {
+      bookmarks: []
+    }
+  },
   components: {
     Spell
+  },
+  methods: {
+    addBookmark (spell) {
+      this.bookmarks.push(spell)
+    },
+    removeBookmark (spell) {
+      const index = this.bookmarks.indexOf(spell)
+      this.bookmarks.splice(index, 1)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+  button {
+    cursor: pointer;
+  }
 </style>
