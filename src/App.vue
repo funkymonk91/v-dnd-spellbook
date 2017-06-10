770c67084a38e8bd1cd634948e62e96c0ff83ccb
Sean <s.frankruyter@gmail.com>
2017-06-10 13:30:41 -0400

    Init SpellList displays Spells Spells can toggle details spells.json is NOT included due to copyright reasons from WOTC

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <spellList v-bind:spells="spells"></spellList>
+  </div>
+</template>
+
+<script>
+import spells from './data/spells.json'
+import SpellList from './components/SpellList'
+
+export default {
+  name: 'app',
+  components: {
+    SpellList
+  },
+  data () {
+    return {
+      spells: spells
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SpellList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+      <spell v-for="(spell, index) in spells" v-bind:spell="spell" v-bind:key="index"></spell>
+  </div>
+</template>
+
+<script>
+import Spell from './Spell'
+
+export default {
+  props: ['spells'],
+  components: {
+    Spell
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
