db71f54a3f7fb8214b290ca3b8ddea90cae161ec
Sean <s.frankruyter@gmail.com>
2017-06-11 12:28:22 -0400

    Spells can now be bookmarked and will persist in localstorage

--- src/components/SpellList.vue
+++ src/components/SpellList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-      <spell v-for="(spell, index) in spells" v-bind:spell="spell" v-bind:key="index" v-on:add-bookmark="addBookmark" v-on:remove-bookmark="removeBookmark"></spell>
+      <spell v-for="(spell, index) in spells" v-bind:spell="spell" v-bind:key="index" v-bind:bookmarked="isSpellBookmarked(spell)" v-on:add-bookmark="addBookmark" v-on:remove-bookmark="removeBookmark"></spell>
   </div>
 </template>
 
@@ -17,13 +17,31 @@ export default {
   components: {
     Spell
   },
+  created: function () {
+    this.loadBookmarks()
+  },
   methods: {
+    loadBookmarks () {
+      if(localStorage['bookmarks'] !== undefined) {
+        console.log('loadBookmarks');
+        this.bookmarks = JSON.parse(localStorage['bookmarks'])
+      }
+    },
     addBookmark (spell) {
-      this.bookmarks.push(spell)
+      this.bookmarks.push(spell.name)
+      // Persist the bookmarks
+      localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks))
     },
     removeBookmark (spell) {
-      const index = this.bookmarks.indexOf(spell)
+      const index = this.bookmarks.indexOf(spell.name)
       this.bookmarks.splice(index, 1)
+      // Persist the bookmarks
+      localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks))
+    },
+    isSpellBookmarked (spell) {
+      const index = this.bookmarks.indexOf(spell.name)
+      const returnVal = (index !== -1) ? true : false
+      return returnVal
     }
   }
 }
